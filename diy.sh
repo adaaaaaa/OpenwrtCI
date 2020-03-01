b1389944cbc447b4cf6a92adfa0e0c25d39ed1e4
adaaaaaa <15017597525@163.com>
2020-03-01 22:43:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,11 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.100.252/g' package/base-files/files/bin/config_generate
+
+# Modify the version number
+sed -i 's/OpenWrt/holahola build $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
